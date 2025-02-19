a91f36a21fdcf5d47a29fbc71ff5cf99dac9439f
momin109 <momin550550@gmail.com>
2025-02-19 21:12:41 +0600

    Implement-Add-To-Cart

--- src/components/shared/header/menu.tsx
+++ src/components/shared/header/menu.tsx
@@ -1,19 +1,10 @@
-import { ShoppingCartIcon, UserIcon } from "lucide-react";
-import Link from "next/link";
+import CartButton from "./cart-button";
 
 export default function Menu() {
   return (
     <div className="flex justify-end">
       <nav className="flex gap-3 w-full">
-        <Link href="/cart" className="header-button">
-          <UserIcon className="h-8 w-8" />
-          <span className="font-bold">Sign in</span>
-        </Link>
-
-        <Link href="/cart" className="header-button">
-          <ShoppingCartIcon className="h-8 w-8" />
-          <span className="font-bold">Cart</span>
-        </Link>
+        <CartButton />
       </nav>
     </div>
   );
