33feda34950e76b4090e675b4e38a19b444cdc02
momin109 <momin550550@gmail.com>
2025-02-22 07:24:38 +0600

    place-order

--- src/app/checkout/checkout-form.tsx
+++ src/app/checkout/checkout-form.tsx
@@ -42,6 +42,8 @@ import {
   AVAILABLE_PAYMENT_METHODS,
   DEFAULT_PAYMENT_METHOD,
 } from "@/lib/constants";
+import { createOrder } from "@/lib/actions/order.actions";
+import { toast } from "@/hooks/use-toast";
 
 const shippingAddressDefaultValues =
   process.env.NODE_ENV === "development"
@@ -81,6 +83,7 @@ const CheckoutForm = () => {
     setShippingAddress,
     setPaymentMethod,
     updateItem,
+    clearCart,
     removeItem,
     setDeliveryDateIndex,
   } = useCartStore();
@@ -113,7 +116,32 @@ const CheckoutForm = () => {
     useState<boolean>(false);
 
   const handlePlaceOrder = async () => {
-    // TODO: place order
+    const res = await createOrder({
+      items,
+      shippingAddress,
+      expectedDeliveryDate: calculateFutureDate(
+        AVAILABLE_DELIVERY_DATES[deliveryDateIndex!].daysToDeliver
+      ),
+      deliveryDateIndex,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      taxPrice,
+      totalPrice,
+    });
+    if (!res.success) {
+      toast({
+        description: res.message,
+        variant: "destructive",
+      });
+    } else {
+      toast({
+        description: res.message,
+        variant: "default",
+      });
+      clearCart();
+      router.push(`/checkout/${res.data?.orderId}`);
+    }
   };
   const handleSelectPaymentMethod = () => {
     setIsAddressSelected(true);
